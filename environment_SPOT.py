003b7e79369f3c3da953407792a2253da19aeff7
Kirkados <khovell@gmail.com>
2021-01-08 09:03:23 -0500

    replay buffer makes agents wait until its save is complete

--- environment_SPOT.py
+++ environment_SPOT.py
@@ -96,7 +96,7 @@ class Environment:
         self.MIN_V                            = -100.
         self.MAX_V                            =  125.
         self.N_STEP_RETURN                    =   5
-        self.DISCOUNT_FACTOR                  =   0.97**(1/self.N_STEP_RETURN)
+        self.DISCOUNT_FACTOR                  =   0.99**(1/self.N_STEP_RETURN)
         self.TIMESTEP                         =   0.2 # [s]
         self.DYNAMICS_DELAY                   =   0 # [timesteps of delay] how many timesteps between when an action is commanded and when it is realized
         self.AUGMENT_STATE_WITH_ACTION_LENGTH =   0 # [timesteps] how many timesteps of previous actions should be included in the state. This helps with making good decisions among delayed dynamics.

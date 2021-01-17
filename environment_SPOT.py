fixed target rotation bug

environment_SPOT.py
@@ -92,7 +92,7 @@ class Environment:
         self.RANDOMIZATION_LENGTH             = 0.5 # [m] half-range uniform randomization position
         self.RANDOMIZATION_ANGLE              = np.pi # [rad] half-range uniform randomization chaser and target angle
         self.RANDOMIZATION_TARGET_VELOCITY    = 0.0 # [m/s] half-range uniform randomization target velocity
-        self.RANDOMIZATION_TARGET_OMEGA       = 0.0 # [rad/s] half-range uniform randomization target omega
+        self.RANDOMIZATION_TARGET_OMEGA       = 2*np.pi/30 # [rad/s] half-range uniform randomization target omega
         self.MIN_V                            = -100.
         self.MAX_V                            =  125.
         self.N_STEP_RETURN                    =   5
@@ -285,7 +285,7 @@ class Environment:
 
 
         # Step target's state ahead one timestep
-        self.target_position += self.INITIAL_TARGET_VELOCITY * self.TIMESTEP
+        self.target_position += self.target_velocity * self.TIMESTEP
 
         # Update docking locations
         self.update_docking_locations()
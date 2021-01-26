7051fff5167035cc2ff52010b036ccfd1ce924ff
Kirkados <khovell@gmail.com>
2021-01-26 12:32:15 -0500

    shrunk the mid-way reward radius from 0.3 m to 0.1 m

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
     ##### Run Settings #####
     ########################
 
-    RUN_NAME               = 'stopOnCollide_noSpin_morePenalty'#'velocity_penalty50_gamma95_4piend_random_uniform'
+    RUN_NAME               = 'stopOnCollide_r0p1_5collide_noSpin'#'velocity_penalty50_gamma95_4piend_random_uniform'
     RESUME_TRAINING        = False # If True, main.py must be run from a previous run's 'code' folder.
     ENVIRONMENT            = 'SPOT'
     AGENT                  = ''

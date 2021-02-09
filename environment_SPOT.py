f603ddfb99a4a342230aa1808b81efc285e1442d
Kirkados <khovell@gmail.com>
2021-02-09 15:58:24 -0500

    the chaser true state is now included to make the system Markovian

--- environment_SPOT.py
+++ environment_SPOT.py
@@ -74,8 +74,10 @@ class Environment:
         #         The TOTAL_STATE is passed to the animator below to animate the motion.
         #         The chaser and target state are contained in the environment. They are packaged up before being returned to the agent.
         #         The total state information returned must be as commented beside self.TOTAL_STATE_SIZE.
-        #self.IRRELEVANT_STATES                = [6,7,8,9,10,11,12,13,14,15,16,17] # indices of states who are irrelevant to the policy network
-        self.IRRELEVANT_STATES                = [6,7,9,10,11,12,13,14,15,16,17]
+        #self.IRRELEVANT_STATES                = [6,7,8,9,10,11,12,13,14,15,16,17] # [original] indices of states who are irrelevant to the policy network
+        self.IRRELEVANT_STATES                = [2,3,5,9,10,11] # [chaser_x, chaser_y, chaser_theta, relative_x, relative_y, relative_angle, chaser_vx, chaser_vy, chaser_omega, target_vx, target_vy, target_omega]
+        #self.IRRELEVANT_STATES                = [2,3,5,6,7,9,10,11] # [chaser_theta, relative_x, relative_y, relative_angle, chaser_vx, chaser_vy, chaser_omega, target_vx, target_vy, target_omega] # omitting chaser_x, chaser_y
+        #self.IRRELEVANT_STATES                = [2,3,5,6,7,9,10,11,15,16] # [chaser_theta, relative_x, relative_y, relative_angle, chaser_vx, chaser_vy, chaser_omega, target_omega] # omitting chaser_x, chaser_y, target_vx, target_vy
         self.OBSERVATION_SIZE                 = self.TOTAL_STATE_SIZE - len(self.IRRELEVANT_STATES) # the size of the observation input to the policy
         self.ACTION_SIZE                      = 3 # [x_dot_dot, y_dot_dot, theta_dot_dot] in the BODY frame
         self.MAX_VELOCITY                     = 0.5 # [m/s]
--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
     ##### Run Settings #####
     ########################
 
-    RUN_NAME               = 'endOnCollide_5penalty_r0p1_-100-125bounds_Spin_chaserAngle'#'velocity_penalty50_gamma95_4piend_random_uniform'
+    RUN_NAME               = '5penalty_fullState_spin'#'velocity_penalty50_gamma95_4piend_random_uniform'
     RESUME_TRAINING        = False # If True, main.py must be run from a previous run's 'code' folder.
     ENVIRONMENT            = 'SPOT'
     AGENT                  = ''

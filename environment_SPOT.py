56810224927ca53da457a910e7f23854354e9903
Kirkados <khovell@gmail.com>
2021-02-14 10:49:29 -0500

    finalizing the state that works for the rotating experiment

--- environment_SPOT.py
+++ environment_SPOT.py
@@ -76,8 +76,8 @@ class Environment:
         #         The total state information returned must be as commented beside self.TOTAL_STATE_SIZE.
         #self.IRRELEVANT_STATES                = [6,7,8,9,10,11,12,13,14,15,16,17] # [original] indices of states who are irrelevant to the policy network
         #self.IRRELEVANT_STATES                = [2,3,5,9,10,11] # [chaser_x, chaser_y, chaser_theta, relative_x, relative_y, relative_angle, chaser_vx, chaser_vy, chaser_omega, target_vx, target_vy, target_omega]
-        self.IRRELEVANT_STATES                = [2,3,5,6,7,9,10,11] # [chaser_theta, relative_x, relative_y, relative_angle, chaser_vx, chaser_vy, chaser_omega, target_vx, target_vy, target_omega] # omitting chaser_x, chaser_y
-        #self.IRRELEVANT_STATES                = [2,3,5,6,7,9,10,11,15,16] # [chaser_theta, relative_x, relative_y, relative_angle, chaser_vx, chaser_vy, chaser_omega, target_omega] # omitting chaser_x, chaser_y, target_vx, target_vy
+        #self.IRRELEVANT_STATES                = [2,3,5,6,7,9,10,11] # [chaser_theta, relative_x, relative_y, relative_angle, chaser_vx, chaser_vy, chaser_omega, target_vx, target_vy, target_omega] # omitting chaser_x, chaser_y
+        self.IRRELEVANT_STATES                = [2,3,5,6,7,9,10,11,15,16] # [chaser_theta, relative_x, relative_y, relative_angle, chaser_vx, chaser_vy, chaser_omega, target_omega] # omitting chaser_x, chaser_y, target_vx, target_vy
         self.OBSERVATION_SIZE                 = self.TOTAL_STATE_SIZE - len(self.IRRELEVANT_STATES) # the size of the observation input to the policy
         self.ACTION_SIZE                      = 3 # [x_dot_dot, y_dot_dot, theta_dot_dot] in the BODY frame
         self.MAX_VELOCITY                     = 0.5 # [m/s]
